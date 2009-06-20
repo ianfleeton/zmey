6dc873d18214f2e6fc42cea4cebf7d171d92fd8e
Ian Fleeton <ianfleeton@gmail.com>
2009-06-20 16:49:49 +0100

    Validations for Google Analytics Code

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,4 +1,7 @@
 class Website < ActiveRecord::Base
+  validates_uniqueness_of :google_analytics_code
+  validates_format_of :google_analytics_code, :with => /\AUA-\d\d\d\d\d\d\d-\d\Z/
+  
   def self.for(domain, subdomains)
     website = find_by_domain(domain)
     unless subdomains.blank?
