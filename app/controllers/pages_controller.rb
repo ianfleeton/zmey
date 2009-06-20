5249078fe67da65ccfaedbb12fc14ec74db9ec79
Ian Fleeton <ianfleeton@gmail.com>
2009-06-20 17:05:01 +0100

    Show titles for pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,7 @@ class PagesController < ApplicationController
 
   def show
     @page = Page.find_by_slug_and_website_id(params[:slug], @w)
+    @title = @page.title
     render :file => "#{RAILS_ROOT}/public/404.html", :status => "404 Not Found" if @page.nil?
   end
   
