ab7ed3db737a473f8327b15f6fb16f6b5ec7c66e
Ian Fleeton <ianfleeton@gmail.com>
2009-06-29 17:55:44 +0100

    Page name and title uniqueness validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,6 +3,8 @@ class Page < ActiveRecord::Base
   validates_presence_of :title, :name, :slug, :keywords, :description
   validates_format_of :slug, :with => /\A[-a-z0-9]+\Z/, :message => 'can only contain letters, numbers and hyphens'
   validates_uniqueness_of :slug, :scope => :website_id, :case_sensitive => false
+  validates_uniqueness_of :title, :scope => :website_id, :case_sensitive => false
+  validates_uniqueness_of :name, :scope => :website_id, :case_sensitive => false
 
   def to_param
     slug
