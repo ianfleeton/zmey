cde1cc56579d5b7899b5016a5c28e9d12dd26298
Ian Fleeton <ianfleeton@gmail.com>
2009-07-24 22:57:05 +0100

    Allow 6 as well as 7 digits in main part of Google Analytics code - some older codes have only 6 digits

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,6 +1,6 @@
 class Website < ActiveRecord::Base
   validates_uniqueness_of :google_analytics_code
-  validates_format_of :google_analytics_code, :with => /\AUA-\d\d\d\d\d\d\d-\d\Z/
+  validates_format_of :google_analytics_code, :with => /\AUA-\d\d\d\d\d\d(\d)?-\d\Z/
   
   def self.for(domain, subdomains)
     website = find_by_domain(domain)
