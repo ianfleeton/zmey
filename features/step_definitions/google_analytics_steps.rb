66e5062a8c5a3ae364cc5b8cfad20ca11698c82f
Ian Fleeton <ianfleeton@gmail.com>
2009-07-24 18:56:00 +0100

    Cucumber Google Analytics feature

---
+++ features/step_definitions/google_analytics_steps.rb
@@ -0,0 +1,15 @@
+Given /^a website without tracking code$/ do
+  @host = websites(:website_without_analytics).domain
+end
+
+Given /^a website with tracking code$/ do
+  @host = websites(:guitar_gear).domain
+end
+
+Then /^I should find the correct tracking code$/ do
+  response.should contain(websites(:website_without_analytics).google_analytics_code)
+end
+
+Then /^I should not find tracking code$/ do
+  response.should_not contain('google-analytics.com')
+end
