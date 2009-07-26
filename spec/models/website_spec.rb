e60fe26f48ebd9860ee5d958b8b20f5499b7143e
Ian Fleeton <ianfleeton@gmail.com>
2009-07-26 14:16:30 +0100

    Model spec for Website

---
+++ spec/models/website_spec.rb
@@ -0,0 +1,26 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper') 
+
+describe Website do 
+  describe "validations" do
+    before(:each) do
+      @website = Website.new(
+        :subdomain => 'bonsai',
+        :domain => 'www.artofbonsai.com',
+        :name => 'Art of Bonsai',
+        :email => 'artofbonsai@example.org',
+        :google_analytics_code => 'UA-9999999-9')
+    end
+
+    it "should be valid with valid attributes" do
+      @website.should be_valid
+    end
+
+    it "should require a name" do
+      @website.name = nil
+      @website.should_not be_valid
+    end
+
+    it "should keep Google Analytics codes unique"
+
+  end
+end
