796915923b0332ff060607d8f281a2d3a72453e4
Ian Fleeton <ianfleeton@gmail.com>
2009-07-29 16:48:25 +0100

    Sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @current_user = User.authenticate(params[:email], params[:password])
+    if @current_user
+      session[:user] = @current_user.id
+      redirect_to :controller => "users", :action => "show",
+                  :id => @current_user.id
+    else
+      flash[:notice] = "No user was found with this email/password"
+      redirect_to :action => "new"
+    end
+  end
+
+  def destroy
+    reset_session
+    
+    flash[:notice] = "Logged out successfully"
+    redirect_to :action => "new"
+  end
+end
