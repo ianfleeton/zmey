aae2ddf957e21d9c3e679e1812106fb1f3032a7d
Ian Fleeton <ianfleeton@gmail.com>
2009-07-29 16:55:41 +0100

    Users Controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,113 @@
+class UsersController < ApplicationController
+  before_filter :find_user, :except => [:index, :new, :create, :forgot_password, :forgot_password_send]
+  before_filter :admin_or_same_user_required, :only => [:show, :edit]
+  before_filter :admin_required, :only => [:index, :destroy]
+
+  def index
+    @users = User.find(:all, :order => "name")
+  end
+  
+  def show
+    @title = 'Your Account'
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    @user.admin = false
+    
+    if @user.save
+      unless admin?
+        @current_user  = @user
+        session[:user] = @user.id
+      end
+
+      flash[:notice] = "Your account has been created."
+      redirect_to :action => "show", :id => @user.id
+    else
+      render :action => "new"
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    # only administrators can update protected attributes
+    if admin?
+      @user.admin = params[:user][:admin]
+      @user.free_search = params[:user][:free_search]
+    end
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "User successfully updated."
+      redirect_to :action => "show", :id => @user.id
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:notice] = "User successfully destroyed."
+    redirect_to :action => "index"
+  end
+
+  def forgot_password
+  end
+  
+  def forgot_password_send
+    @user = User.find_by_email(params[:email])
+    if @user.nil?
+      flash[:notice] = "There is no user registered with that email address"
+      redirect_to :action => "forgot_password"
+    else
+      @user.forgot_password_token = User.generate_forgot_password_token
+      @user.save
+      UserNotifier.deliver_token @user
+    end
+  end
+
+  def forgot_password_new
+    forgot_password_params_ok?
+  end
+  
+  def forgot_password_change
+    if forgot_password_params_ok?
+      @user.password = params[:password]
+      @user.forgot_password_token = ''
+      @user.save
+      flash[:notice] = 'Your password has been changed'
+      redirect_to :controller => 'sessions', :action => 'new'
+    end
+  end
+  
+  private
+  
+  def forgot_password_params_ok?
+    if @user.forgot_password_token.blank?
+      flash[:notice] = "Please enter your email address below"
+      redirect_to :action => "forgot_password"
+      return false
+    elsif params[:t].nil? or @user.forgot_password_token != params[:t]
+      flash[:notice] = "The link you entered was invalid. This can happen if you have re-requested " +
+        "a forgot password email or you have already reset and changed your password."
+      redirect_to :action => "forgot_password"
+      return false
+    end
+    true
+  end
+  
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def admin_or_same_user_required
+    if !admin? and (!logged_in? or @current_user != @user)
+      redirect_to :controller => 'sessions', :action => 'new'
+      return
+    end
+  end
+end
