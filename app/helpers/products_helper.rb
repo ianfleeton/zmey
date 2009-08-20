6659febf255d3f12f892ba85b0f76119f9329d8d
Ian Fleeton <ianfleeton@gmail.com>
2009-08-20 20:08:21 +0100

    Showing basic product information in pages (name, price, image, description) with some admin controls (add product to page; remove product from page)

---
+++ app/helpers/products_helper.rb
@@ -0,0 +1,5 @@
+module ProductsHelper
+  def formatted_price price
+    number_to_currency(price, :unit => '&pound;')
+  end
+end
--- app/models/website.rb
+++ app/models/website.rb
@@ -3,6 +3,8 @@ class Website < ActiveRecord::Base
   validates_format_of :google_analytics_code, :with => /\AUA-\d\d\d\d\d\d(\d)?-\d\Z/, :allow_blank => true
   validates_presence_of :name
   
+  has_many :products, :order => :name
+  
   def self.for(domain, subdomains)
     website = find_by_domain(domain)
     unless subdomains.blank?
