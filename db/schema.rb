3c0630e60e96e731ad5a644ff5961e3775407c34
Ian Fleeton <ianfleeton@gmail.com>
2009-09-10 16:21:20 +0100

    Add feature_descriptions to basket_items

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090909122226) do
+ActiveRecord::Schema.define(:version => 20090910151447) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
@@ -26,35 +26,13 @@ ActiveRecord::Schema.define(:version => 20090909122226) do
     t.string   "email_address",  :default => "", :null => false
   end
 
-  create_table "feature_selections", :force => true do |t|
-    t.integer  "basket_item_id", :default => 0,     :null => false
-    t.integer  "feature_id",   :default => 0,     :null => false
-    t.integer  "choice_id"
-    t.text     "customer_text",                     :null => false
-    t.boolean  "checked",        :default => false, :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "feature_selections", ["basket_item_id"], :name => "index_feature_selections_on_basket_item_id"
-
-  create_table "features", :force => true do |t|
-    t.integer  "product_id"
-    t.string   "name",       :default => "",   :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "ui_type",    :default => 0,    :null => false
-    t.boolean  "required",   :default => true, :null => false
-  end
-
-  add_index "features", ["product_id"], :name => "index_features_on_product_id"
-
   create_table "basket_items", :force => true do |t|
-    t.integer  "basket_id",  :default => 0, :null => false
-    t.integer  "product_id", :default => 0, :null => false
-    t.integer  "quantity",   :default => 1, :null => false
+    t.integer  "basket_id",            :default => 0, :null => false
+    t.integer  "product_id",           :default => 0, :null => false
+    t.integer  "quantity",             :default => 1, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "feature_descriptions"
   end
 
   create_table "baskets", :force => true do |t|
@@ -69,7 +47,7 @@ ActiveRecord::Schema.define(:version => 20090909122226) do
     t.datetime "updated_at"
   end
 
-  add_index "choices", ["feature_id"], :name => "index_choices_on_feature_id"
+  add_index "choices", ["feature_id"], :name => "index_choices_on_attribute_id"
 
   create_table "countries", :force => true do |t|
     t.string   "name",               :default => "", :null => false
@@ -93,6 +71,29 @@ ActiveRecord::Schema.define(:version => 20090909122226) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "feature_selections", :force => true do |t|
+    t.integer  "basket_item_id", :default => 0,     :null => false
+    t.integer  "feature_id",     :default => 0,     :null => false
+    t.integer  "choice_id"
+    t.text     "customer_text",                     :null => false
+    t.boolean  "checked",        :default => false, :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "feature_selections", ["basket_item_id"], :name => "index_attribute_selections_on_basket_item_id"
+
+  create_table "features", :force => true do |t|
+    t.integer  "product_id"
+    t.string   "name",       :default => "",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "ui_type",    :default => 0,    :null => false
+    t.boolean  "required",   :default => true, :null => false
+  end
+
+  add_index "features", ["product_id"], :name => "index_attributes_on_product_id"
 
   create_table "images", :force => true do |t|
     t.integer  "website_id", :default => 0,  :null => false
