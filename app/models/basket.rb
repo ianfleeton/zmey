17ddf5f93306f4e99fe718746eb0b808c38243de
Ian Fleeton <ianfleeton@gmail.com>
2009-09-10 14:09:09 +0100

    Method to calculate basket total; destroy rather than delete_all basket_items

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,3 +1,9 @@
 class Basket < ActiveRecord::Base
-  has_many :basket_items, :dependent => :delete_all
+  # basket items are destroyed so that their feature selections can be cleaned up
+  has_many :basket_items, :dependent => :destroy
+  def total
+    total = 0.0
+    basket_items.each {|i| total += i.line_total}
+    total
+  end
 end
