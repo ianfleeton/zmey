d437340295071a5dd683e0f3f883f42b1fb06d4d
Ian Fleeton <ianfleeton@gmail.com>
2009-09-30 13:15:26 +0100

    Cucumber update and path for create account

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,24 +1,21 @@
 module NavigationHelpers
-  # Maps a static name to a static route.
+  # Maps a name to a path. Used by the
   #
-  # This method is *not* designed to map from a dynamic name to a 
-  # dynamic route like <tt>post_comments_path(post)</tt>. For dynamic 
-  # routes like this you should *not* rely on #path_to, but write 
-  # your own step definitions instead. Example:
+  #   When /^I go to (.+)$/ do |page_name|
   #
-  #   Given /I am on the comments page for the "(.+)" post/ |name|
-  #     post = Post.find_by_name(name)
-  #     visit post_comments_path(post)
-  #   end
+  # step definition in webrat_steps.rb
   #
   def path_to(page_name)
     case page_name
     
     when /the homepage/
-      root_path
+      '/'
     
-    # Add more page name => path mappings here
-    
+    # Add more mappings here.
+    # Here is a more fancy example:
+    #
+    #   when /^(.*)'s profile page$/i
+    #     user_profile_path(User.find_by_login($1))
     when /the checkout page/
       url_for :controller => 'basket', :action => 'checkout', :only_path => true
 
@@ -27,7 +24,10 @@ module NavigationHelpers
 
     when /the login page/
       url_for :controller => 'sessions', :action => 'new', :only_path => true
-    
+
+    when /the create account page/
+      url_for :controller => 'users', :action => 'new', :only_path => true
+  
     when /the new page page/
       url_for :controller => 'pages', :action => 'new', :only_path => true
 
@@ -36,7 +36,7 @@ module NavigationHelpers
 
     when /the list websites page/
       url_for :controller => 'websites', :only_path => true
-    
+  
     when /the new website page/
       new_website_path
 
@@ -45,7 +45,7 @@ module NavigationHelpers
 
     else
       raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-        "Now, go and add a mapping in features/support/paths.rb"
+        "Now, go and add a mapping in #{__FILE__}"
     end
   end
 end
