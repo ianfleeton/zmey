6547ca41bffc3646557b4aaae7c4a4013036c25a
Ian Fleeton <ianfleeton@gmail.com>
2009-11-02 21:11:47 +0000

    Gravatar helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,5 +6,12 @@ module UsersHelper
     end 
   end 
 
+  def gravatar(email_address)
+    require 'digest/md5'
+    email_address.downcase!
+    hash = Digest::MD5.hexdigest(email_address)
+    image_src = "http://www.gravatar.com/avatar/#{hash}" + '?d=identicon'
+    image_tag(image_src, :alt => 'Gravatar')
+  end
 
 end
