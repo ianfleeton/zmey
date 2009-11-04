754bdf463ea4136449af0ffceb1e900e7e3dace0
Ian Fleeton <ianfleeton@gmail.com>
2009-11-04 15:19:41 +0000

    Format addresses for plain text as well as HTML

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -1,16 +1,23 @@
 module AddressesHelper
   # formats an address in HTML
-  def format_address a
-    content_tag(
-      :p,
-      h(a.full_name) +
-      ' &lt;' + h(a.email_address) + '&gt;<br />' + 
-      h(a.address_line_1) + '<br />' +
-      (a.address_line_2.empty? ? '' : h(a.address_line_2) + '<br />') +
+  def format_address a, html = true
+    if html
+      lt = '&lt;'
+      gt = '&gt;'
+      lb = '<br />'
+    else
+      lt = '<'
+      gt = '>'
+      lb = "\n"
+    end
+    address = h(a.full_name) +
+      ' ' + lt + h(a.email_address) + gt + lb + 
+      h(a.address_line_1) + lb +
+      (a.address_line_2.empty? ? '' : h(a.address_line_2) + lb) +
       h(a.town_city) + ', ' +
       (a.county.empty? ? '' : h(a.county) + ', ') +
       h(a.postcode) +
-      (a.phone_number.empty? ? '' : '<br />' + h(a.phone_number))
-    )
+      (a.phone_number.empty? ? '' : lb + h(a.phone_number))
+    html ? content_tag(:p, address) : address
   end
 end
