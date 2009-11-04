9bf8f6bb3f02a8f8625aeca2268a8468e631930f
Ian Fleeton <ianfleeton@gmail.com>
2009-11-04 15:20:01 +0000

    Helper for formatted prices with GBP as well as £

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,7 @@ module ProductsHelper
   def formatted_price price
     number_to_currency(price, :unit => '&pound;')
   end
+  def formatted_gbp_price price
+    number_to_currency(price, :unit => 'GBP', :format => '%n %u')
+  end
 end
