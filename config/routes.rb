239f66601d72180fdeb666ded1144da9bcbbc97a
Ian Fleeton <ianfleeton@gmail.com>
2009-11-26 12:59:22 +0000

    RESTful routes for orders

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :features
   map.resources :enquiries, :collection => { :thank_you => :get }
   map.resources :websites
+  map.resources :orders, :collection => { :receipt => :get, :select_payment_method => :get, :purge_old_unpaid => :get }
   map.resources :products
   map.resources :product_placements
   map.resources :forums
