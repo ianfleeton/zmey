1ec7345962deb5e98715ab15396e2347b95d9b7c
Ian Fleeton <ianfleeton@gmail.com>
2010-01-22 19:00:27 +0000

    Validate email addresses in user and address models

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,5 @@
 class Address < ActiveRecord::Base
   attr_protected :user_id
   validates_presence_of :full_name, :email_address, :address_line_1, :town_city, :postcode, :country_id
+  validates_format_of :email_address, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
 
   # validation
   validates_length_of     :email, :within => 3..100
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates_uniqueness_of :email, :case_sensitive => false, :message => 'has already been taken. If you have forgotten your password you can request a new one.'
   
   validates_presence_of   :name
