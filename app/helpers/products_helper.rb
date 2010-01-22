d0b0d9e380be37a1ecb1120237f53d47a8d8d983
Ian Fleeton <ianfleeton@gmail.com>
2010-01-22 19:03:25 +0000

    Product helper for tax explanation which is usually displayed next to the product's price to inform the customer what tax is applied to the price shown

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -5,4 +5,17 @@ module ProductsHelper
   def formatted_gbp_price price
     number_to_currency(price, :unit => 'GBP', :format => '%n %u')
   end
+  def tax_explanation(product)
+    if @w.vat_number.empty?
+      ''
+    else
+      if product.tax_type == Product::EX_VAT || product.tax_type == Product::INC_VAT
+        if @inc_tax
+          ' <span class="tax_explanation">inc VAT</span>'
+        else
+          ' <span class="tax_explanation">ex VAT</span>'
+        end
+      end
+    end
+  end
 end
