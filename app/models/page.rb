05afcc031ae8bda5ab058063cf869d78de7fd4d0
Ian Fleeton <ianfleeton@gmail.com>
2010-01-23 19:40:10 +0000

    Validate presence of Page.website_id

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,7 +6,7 @@ class Page < ActiveRecord::Base
   has_many :products, :through => :product_placement
 
   attr_protected :website_id
-  validates_presence_of :title, :name, :keywords, :description
+  validates_presence_of :title, :name, :keywords, :description, :website_id
   validates_format_of :slug, :with => /\A[-a-z0-9]+\Z/, :message => 'can only contain letters, numbers and hyphens', :allow_blank => true
   validates_uniqueness_of :slug, :scope => :website_id, :case_sensitive => false
   validates_uniqueness_of :title, :scope => :website_id, :case_sensitive => false
