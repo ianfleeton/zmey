d9d3d9ea23abe58bbec8cfd9c6c873da50b83f21
Ian Fleeton <ianfleeton@gmail.com>
2010-07-27 16:22:11 +0100

    Order display of main navigation menus by their position

--- app/models/page.rb
+++ app/models/page.rb
@@ -41,7 +41,7 @@ class Page < ActiveRecord::Base
 
   def self.navs website_id
     navs = Array.new
-    nav_roots = Page.all(:conditions => ['website_id = ? AND parent_id IS NULL', website_id])
+    nav_roots = Page.all(:conditions => ['website_id = ? AND parent_id IS NULL', website_id], :order => :position)
     nav_roots.each do |nr|
       nav = Navigation.new
       nav.id_attribute = nr.slug.gsub('-', '_') + '_nav'
