0393095504ab18195c14a968c2d6c7437e0ec4f5
Ian Fleeton <ianfleeton@gmail.com>
2010-10-21 16:32:57 +0100

    Relax unique page name scope to parent_id

--- app/models/page.rb
+++ app/models/page.rb
@@ -11,7 +11,7 @@ class Page < ActiveRecord::Base
   validates_format_of :slug, :with => /\A[-a-z0-9]+\Z/, :message => 'can only contain letters, numbers and hyphens', :allow_blank => true
   validates_uniqueness_of :slug, :scope => :website_id, :case_sensitive => false
   validates_uniqueness_of :title, :scope => :website_id, :case_sensitive => false
-  validates_uniqueness_of :name, :scope => :website_id, :case_sensitive => false
+  validates_uniqueness_of :name, :scope => :parent_id, :case_sensitive => false
 
   def self.bootstrap website
     primary_nav_page = create_navigation website, 'primary'
