3691a4971200cc93db9f90084feb95b6034ad771
Ian Fleeton <ianfleeton@gmail.com>
2010-11-13 02:06:06 +0000

    Conditional validation for unique Page.name
    Only validate if the page has a parent, since those without parents will
    likely have the same name (such as 'Primary Navigation') between websites.

--- app/models/page.rb
+++ app/models/page.rb
@@ -11,7 +11,7 @@ class Page < ActiveRecord::Base
   validates_format_of :slug, :with => /\A[-a-z0-9]+\Z/, :message => 'can only contain letters, numbers and hyphens', :allow_blank => true
   validates_uniqueness_of :slug, :scope => :website_id, :case_sensitive => false
   validates_uniqueness_of :title, :scope => :website_id, :case_sensitive => false
-  validates_uniqueness_of :name, :scope => :parent_id, :case_sensitive => false
+  validates_uniqueness_of :name, :scope => :parent_id, :case_sensitive => false, :unless => Proc.new { |page| page.parent_id.nil? }
 
   def name_with_ancestors
     parent ? parent.name_with_ancestors + ' > ' + name : name
