62e97a98bacb0f76ecbb4dad0b4c52754919aa0a
Ian Fleeton <ianfleeton@gmail.com>
2012-02-24 16:50:09 +0000

    Use recipients= and move to mailers

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -3,7 +3,7 @@ class OrderNotifier < ActionMailer::Base
   helper :addresses # address formatting
 
   def notification website, order
-    recipients [order.email_address, website.email]
+    recipients = [order.email_address, website.email]
     @website = website
     @order = order
     mail(:to => recipients, :subject => website.name + ': order notification ' + order.order_number,
