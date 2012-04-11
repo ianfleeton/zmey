1d02d67ce592785a6ccd2579fb691c0c5bd82d3d
Ian Fleeton <ianfleeton@gmail.com>
2012-04-11 13:56:28 +0100

    Bootstrap shipping_table_rows

--- app/controllers/shipping_table_rows_controller.rb
+++ app/controllers/shipping_table_rows_controller.rb
@@ -1,4 +1,5 @@
 class ShippingTableRowsController < ApplicationController
+  layout 'admin'
   before_filter :admin_or_manager_required
   before_filter :find_shipping_table_row, :only => [:edit, :update, :destroy]
 
