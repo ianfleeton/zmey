6a84915a177fc56f4a7a5c300ff35dcc89900db0
Ian Fleeton <ianfleeton@gmail.com>
2012-06-11 10:33:38 +0100

    Validate presence of forum name

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,5 +1,6 @@
 class Forum < ActiveRecord::Base
   belongs_to :website
   has_many :topics, :order => 'created_at DESC'
+  validates_presence_of :name
   validates_uniqueness_of :name, :scope => :website_id, :case_sensitive => false
 end
