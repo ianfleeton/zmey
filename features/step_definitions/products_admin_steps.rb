f747f78fdaa1aec0dbb2a2e81e25637f01b485f1
Ian Fleeton <ianfleeton@gmail.com>
2012-07-11 13:56:36 +0100

    Don't mass assign website_id

--- features/step_definitions/products_admin_steps.rb
+++ features/step_definitions/products_admin_steps.rb
@@ -1,3 +1,5 @@
 Given /^there is one product in the shop$/ do
-  Product.create!(:website_id => 1, :sku => 'EPI-SG', :name => 'Epiphone SG', :price => '269.99')
+  p = Product.new(sku: 'EPI-SG', name: 'Epiphone SG', price: '269.99')
+  p.website_id = 1
+  p.save!
 end
