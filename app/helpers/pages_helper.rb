4e3332e210ad00f2c5fa73666ddc03c44ac15be9
Ian Fleeton <ianfleeton@gmail.com>
2012-07-20 16:14:30 +0100

    Delete button for pages

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -11,6 +11,7 @@ module PagesHelper
       end
     end
     edit = content_tag(:td, link_to('<i class="icon-edit"></i> Edit'.html_safe, edit_page_path(p), class: 'btn btn-mini'))
+    delete = content_tag(:td, link_to('<i class="icon-trash icon-white"></i> Delete'.html_safe, p, data: { confirm: 'Are you sure?' }, method: :delete, class: 'btn btn-danger btn-mini'))
 
     if p.first?
       move_up = content_tag(:td, '&nbsp;'.html_safe)
@@ -29,6 +30,7 @@ module PagesHelper
         indent_tag(indent) +
           link_to(p.name, p.path) + ' '.html_safe +
           edit +
+          delete +
           move_up +
           move_down
       )
