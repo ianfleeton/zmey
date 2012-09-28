c1be9c657e6df43ec7c7ea9ca4d83157b22af87d
Ian Fleeton <ianfleeton@gmail.com>
2012-09-28 14:12:49 +0100

    Whitelist position

--- app/models/product_placement.rb
+++ app/models/product_placement.rb
@@ -1,5 +1,5 @@
 class ProductPlacement < ActiveRecord::Base
-  attr_accessible :page_id, :product_id
+  attr_accessible :page_id, :position, :product_id
 
   acts_as_list scope: :page
   belongs_to :product
