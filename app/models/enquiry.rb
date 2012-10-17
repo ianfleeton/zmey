502371c03f739e58b4888eb98f748cabc0925524
Ian Fleeton <ianfleeton@gmail.com>
2012-10-17 17:40:46 +0100

    Whitelist enquiry

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -1,5 +1,5 @@
 class Enquiry < ActiveRecord::Base
-  attr_accessible :address, :call_back, :country, :email, :fax, :hear_about, :name, :organisation, :postcode, :telephone, :website_id
+  attr_accessible :address, :call_back, :country, :email, :enquiry, :fax, :hear_about, :name, :organisation, :postcode, :telephone, :website_id
 
   validates_presence_of :name, :telephone, :email, :enquiry
   belongs_to :website
