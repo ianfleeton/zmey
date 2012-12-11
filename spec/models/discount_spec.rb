53cb65f1619dc29fc83e1e210eeeb40986406275
Ian Fleeton <ianfleeton@gmail.com>
2012-12-11 16:36:53 +0000

    Spec Discount#uppercase_coupon_code

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe Discount do
+  describe '#uppercase_coupon_code' do
+    it 'transforms the coupon attribute to uppercase' do
+      discount = Discount.new(coupon: 'Coupon')
+      discount.uppercase_coupon_code
+      discount.coupon.should == 'COUPON'
+    end
+  end
 end
