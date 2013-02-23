271bdb7a11821151863a8a052aba585ed7309915
Ian Fleeton <ianfleeton@gmail.com>
2013-02-23 08:30:31 +0000

    Spec Basket#line_total

--- spec/models/basket_item_spec.rb
+++ spec/models/basket_item_spec.rb
@@ -1,6 +1,28 @@
 require 'spec_helper'
 
 describe BasketItem do
+  describe '#line_total(inc_tax)' do
+    context 'when inc_tax is true' do
+      it 'returns the quantity times the price of the product including tax when buying that quantity' do
+        product = mock_model(Product)
+        product.stub(:price_inc_tax).with(2).and_return(12)
+        item = BasketItem.new(quantity: 2)
+        item.product = product
+        item.line_total(true).should == 24
+      end
+    end
+
+    context 'when inc_tax is false' do
+      it 'returns the quantity times the price of the product excluding tax when buying that quantity' do
+        product = mock_model(Product)
+        product.stub(:price_ex_tax).with(2).and_return(10)
+        item = BasketItem.new(quantity: 2)
+        item.product = product
+        item.line_total(false).should == 20
+      end
+    end
+  end
+
   describe '#weight' do
     it 'returns the weight of the product times the quantity' do
       product = mock_model(Product, {weight: 2.5})
