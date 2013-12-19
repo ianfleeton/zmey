3f111e3dae4b1718aa27fe7110444390b545952c
Ian Fleeton <ianfleeton@gmail.com>
2013-12-19 15:15:51 +0000

    Refactor page_tree to take a block

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,37 +3,51 @@ module PagesHelper
     ('<span class="indent">&nbsp; &nbsp;</span>' * indent).html_safe
   end
 
-  def page_tree p, indent = 0
+  def page_tree(p, indent=0, &block)
     subs = ''
     unless p.children.empty?
       p.children.each do |c|
-        subs += page_tree c, indent+1
+        subs += page_tree(c, indent+1, &block)
       end
     end
-    edit = content_tag(:td, edit_button([:admin, p]))
-    delete = content_tag(:td, delete_button([:admin, p]))
 
-    if p.first?
-      move_up = content_tag(:td, '&nbsp;'.html_safe)
-    else
-      move_up = content_tag(:td, link_to('Move Up', move_up_admin_page(p), class: 'btn btn-mini'))
-    end
+    capture(p, indent, &block) + subs.html_safe
+  end
 
-    if p.last?
-      move_down = content_tag(:td, '&nbsp;'.html_safe)
-    else
-      move_down = content_tag(:td, link_to('Move Down', move_down_admin_page(p), class: 'btn btn-mini'))
-    end
-
+  def page_edit_table_row(page, indent)
     content_tag(:tr,
       content_tag(:td, 
         indent_tag(indent) +
-          link_to(p.name, p.path) + ' '.html_safe +
-          edit +
-          delete +
-          move_up +
-          move_down
-      )
-    ) + subs.html_safe
+          link_to(page.name, page.path) + ' '.html_safe
+        ) +
+        edit_page(page) +
+        move_page_up(page) +
+        move_page_down(page) +
+        delete_page(page)
+    )
+  end
+
+  def edit_page(page)
+    content_tag(:td, edit_button([:admin, page]))
+  end
+
+  def delete_page(page)
+    content_tag(:td, delete_button([:admin, page]))
+  end
+
+  def move_page_up(page)
+    if page.first?
+      content_tag(:td, '&nbsp;'.html_safe)
+    else
+      content_tag(:td, link_to('Move Up', move_up_admin_page_path(page), class: 'btn btn-default'))
+    end
+  end
+
+  def move_page_down(page)
+    if page.last?
+      content_tag(:td, '&nbsp;'.html_safe)
+    else
+      content_tag(:td, link_to('Move Down', move_down_admin_page_path(page), class: 'btn btn-default'))
+    end
   end
 end
