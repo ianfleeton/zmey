9044eec97843fcd36aa1c9efd728a7868fdb449b
Ian Fleeton <ianfleeton@gmail.com>
2014-01-22 20:23:09 +0000

    Junk paths in image zip files

--- lib/image_unzipper.rb
+++ lib/image_unzipper.rb
@@ -4,7 +4,8 @@ class ImageUnzipper
     zip_path = "#{zip_dir}/#{File.basename(zip_file)}"
     FileUtils.makedirs(zip_dir)
     FileUtils.cp(zip_file, zip_path)
-    `unzip #{zip_path} -d #{zip_dir}`
+    # -j junk paths
+    `unzip -j #{zip_path} -d #{zip_dir}`
     Dir.entries(zip_dir).select{|f|File.file?("#{zip_dir}/#{f}") && image?(f)}.each do |f|
       yield ImageFromZip.new("#{zip_dir}/#{f}")
     end
