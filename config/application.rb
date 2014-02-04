65c6a64120c32d0a4c5bda3a169d833affbdd4e2
Ian Fleeton <ianfleeton@gmail.com>
2014-02-04 19:16:14 +0000

    Match new Rails default for I18n.enforce_available_locales

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module YeslWebsite
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = true
 
     config.autoload_paths += Dir["#{config.root}/lib"]
     config.assets.precompile += %w( admin.css screen.css.scss )
