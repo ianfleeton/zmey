ece6749c911e5323d6ea68c546b96db5afad140c
Ian Fleeton <ianfleeton@gmail.com>
2014-02-06 19:40:02 +0000

    Spec PagesController#terms

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe PagesController do
+  describe 'GET terms' do
+    it 'populates @terms from the website config' do
+      controller.stub(:website).and_return(
+        mock_model(Website, terms_and_conditions: 'T&C')
+      )
+      get :terms
+      expect(assigns(:terms)).to eq 'T&C'
+    end
+  end
 end
