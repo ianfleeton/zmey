e32c9424443371111284129d2659cdf6a0f8c5a7
Ian Fleeton <ianfleeton@gmail.com>
2014-04-11 11:52:04 +0100

    Remove image_for_admin_status

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,5 @@
 module UsersHelper
 
-  def image_for_admin_status(user) 
-    if user.admin? 
-      image_tag('iddqd.png', alt: 'Admin', size: '23x24') 
-    end 
-  end 
-
   def gravatar(email_address)
     require 'digest/md5'
     email_address.downcase!
