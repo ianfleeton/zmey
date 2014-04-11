b10ebc6666b2a445801d1e56fe3d54ef99e948f1
Ian Fleeton <ianfleeton@gmail.com>
2014-04-11 11:46:18 +0100

    Require SSL for API in production

--- app/controllers/api/admin/admin_controller.rb
+++ app/controllers/api/admin/admin_controller.rb
@@ -2,6 +2,11 @@ class Api::Admin::AdminController < ApplicationController
   before_action :set_website_for_api
 
   def set_website_for_api
+    if Rails.env.production?
+      # upgrade required if not using SSL
+      render nothing: true, status: 426 and return unless request.ssl?
+    end
+
     key = authenticate_with_http_basic { |u, p| ApiKey.find_by(key: u) }
     if key
       @website = key.user.managed_website
