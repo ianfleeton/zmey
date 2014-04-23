645aeae75df4c02bc3f10b3d73f09a173862ddd4
Ian Fleeton <ianfleeton@gmail.com>
2014-04-23 11:41:01 +0100

    Controller missing from #ab7ad66

---
+++ app/controllers/api/admin/product_placements_controller.rb
@@ -0,0 +1,19 @@
+class Api::Admin::ProductPlacementsController < Api::Admin::AdminController
+  def create
+    @product_placement = ProductPlacement.new(product_placement_params)
+    if Product.exists?(id: params[:product_placement][:product_id], website_id: website.id) && Page.exists?(id: params[:product_placement][:page_id], website_id: website.id)
+      unless @product_placement.save
+        render json: @product_placement.errors, status: :unprocessable_entity
+      end
+    else
+      @product_placement.errors.add(:base, 'Page or product does not exist.')
+      render json: @product_placement.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def product_placement_params
+      params.require(:product_placement).permit(:page_id, :product_id)
+    end
+end
