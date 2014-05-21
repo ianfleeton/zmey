2abe910816abecb84a24535d544ca2c60e630982
Ian Fleeton <ianfleeton@gmail.com>
2014-05-21 13:04:52 +0100

    Generate default label for address

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,10 @@ class Address < ActiveRecord::Base
   validates_presence_of :full_name, :email_address, :address_line_1, :town_city, :postcode, :country_id
   validates_format_of :email_address, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   belongs_to :country
+
+  before_validation :set_default_label
+
+  def set_default_label
+    self.label = "#{full_name} - #{postcode}" if label.blank?
+  end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,4 +1,21 @@
 require 'spec_helper'
 
 describe Address do
+  describe 'before validation' do
+    context 'with label present' do
+      it 'leaves the label untouched' do
+        a = Address.new(label: 'Home')
+        a.save
+        expect(a.label).to eq 'Home'
+      end
+    end
+
+    context 'with label blank' do
+      it 'generates a label for the address based on name and postcode' do
+        a = Address.new(full_name: 'Ian Fleeton', postcode: 'L0N D0N')
+        a.save
+        expect(a.label).to eq 'Ian Fleeton - L0N D0N'
+      end
+    end
+  end
 end
