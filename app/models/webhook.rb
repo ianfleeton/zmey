b6667f9261c036e1a1e2274a94a0c4cee28c30fc
Ian Fleeton <ianfleeton@gmail.com>
2014-06-21 23:03:38 +0100

    Trigger webhook

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -6,4 +6,11 @@ class Webhook < ActiveRecord::Base
   validates_inclusion_of :event, in: EVENTS
   validates_presence_of :url
   validates_presence_of :website_id
+
+  def trigger(payload)
+    uri = URI(url)
+    Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      http.post(uri.request_uri, payload, {'Content-Type' => 'application/json'})
+    end
+  end
 end
