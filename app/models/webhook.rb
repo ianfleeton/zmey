aed7656435b9369c66741b5cca67e1b16686dbeb
Ian Fleeton <ianfleeton@gmail.com>
2014-06-21 23:02:40 +0100

    Validate webhook event inclusion in list

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -1,7 +1,9 @@
 class Webhook < ActiveRecord::Base
   belongs_to :website
 
-  validates_presence_of :event
+  EVENTS = %w(image_created)
+
+  validates_inclusion_of :event, in: EVENTS
   validates_presence_of :url
   validates_presence_of :website_id
 end
