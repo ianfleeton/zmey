fe771f2eccc90668f75a6c5c7244bb6c51b2995c
Ian Fleeton <ianfleeton@gmail.com>
2014-08-29 16:44:13 +0100

    DRY prepare_api_website

--- spec/requests/api/admin/product_placements_spec.rb
+++ spec/requests/api/admin/product_placements_spec.rb
@@ -1,15 +1,8 @@
 require 'rails_helper'
 
-def prepare_api_website
-  @website = FactoryGirl.create(:website)
-  @api_user = FactoryGirl.create(:user, manages_website_id: @website.id)
-  @api_key = FactoryGirl.create(:api_key, user_id: @api_user.id)
-end
-
 describe 'Admin product placements API' do
   before do
     prepare_api_website
-    allow_any_instance_of(Api::Admin::AdminController).to receive(:authenticated_api_key).and_return(@api_key)
   end
 
   describe 'POST create' do
