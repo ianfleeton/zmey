c02c2edf19756f6d7a793cf6178d9258c034f015
Ian Fleeton <ianfleeton@gmail.com>
2014-09-25 13:31:46 +0100

    Protect from forgery with :null_session instead

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery
 
   helper_method :website, :logged_in?, :admin?, :admin_or_manager?, :manager?
 
