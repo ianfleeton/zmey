b3648e3914b29daa7677bddb783313f03ae4d227
Ian Fleeton <ianfleeton@gmail.com>
2014-10-17 14:53:47 +0100

    Improve performance of cached sitemap.xml requests
    700ms+ (not in views/AR) -> 10ms with 4k products and 1k pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,11 +10,11 @@ class PagesController < ApplicationController
   end
 
   def sitemap
-    @pages = Array.new
-    @pages.concat website.pages.select(:id, :parent_id, :slug, :website_id).where(no_index: false).where('parent_id IS NOT NULL')
-    @pages.concat website.products.select(:id, :name, :website_id)
+    pages = website.pages.select(:id, :parent_id, :slug, :updated_at, :website_id).where(no_index: false).where('parent_id IS NOT NULL')
+    products = website.products.select(:id, :name, :updated_at, :website_id)
+    @collections = [pages, products]
     @other_urls = ['/enquiries/new', '/users/forgot_password', '/users/new',
-      '/sessions/new', '/basket', '/pages/terms'].collect{|x| 'http://' + website.domain + x}
+      '/sessions/new', '/basket', '/pages/terms'].collect{|x| website.url + x}
 
     respond_to do |format|
       format.xml { render layout: false }
