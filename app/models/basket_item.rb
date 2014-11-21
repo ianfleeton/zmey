aa2f56eb1cea417f526b455cea2030c43a8011d1
Ian Fleeton <ianfleeton@gmail.com>
2014-11-21 15:49:13 +0000

    basket_id is required

--- app/models/basket_item.rb
+++ app/models/basket_item.rb
@@ -1,5 +1,7 @@
 class BasketItem < ActiveRecord::Base
   validates_numericality_of :quantity, greater_than_or_equal_to: 1
+  validates_presence_of :basket_id
+
   belongs_to :basket, inverse_of: :basket_items, touch: true
   belongs_to :product
   has_many :feature_selections, -> { order 'id' }, dependent: :delete_all
