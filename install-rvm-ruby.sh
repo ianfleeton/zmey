6e896bce4ba49ba3887da824b0e3e0a294ffb424
Ian Fleeton <ianfleeton@gmail.com>
2014-12-05 12:21:15 +0000

    Install mpapis public key [ci skip]

--- install-rvm-ruby.sh
+++ install-rvm-ruby.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/vagrant/.rvm/scripts/rvm
 rvm --default use --install 2.1.5
