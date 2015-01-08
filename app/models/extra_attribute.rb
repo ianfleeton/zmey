47063fd6d9cd9bf5433c1387e14105ed193be457
Ian Fleeton <ianfleeton@gmail.com>
2015-01-08 13:44:49 +0000

    Add attribute name uniqueness constraint

--- app/models/extra_attribute.rb
+++ app/models/extra_attribute.rb
@@ -1,4 +1,6 @@
 class ExtraAttribute < ActiveRecord::Base
+  validates_uniqueness_of :attribute_name, scope: :class_name
+
   # Returns 'ClassName#attribute_name'.
   def to_s
     "#{class_name}##{attribute_name}"
--- spec/models/extra_attribute_spec.rb
+++ spec/models/extra_attribute_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ExtraAttribute, type: :model do
+  it { should validate_uniqueness_of(:attribute_name).scoped_to(:class_name) }
+
   describe '#to_s' do
     it 'returns ClassName#attribute_name' do
       expect(ExtraAttribute.new(attribute_name: 'volume', class_name: 'Product').to_s)
