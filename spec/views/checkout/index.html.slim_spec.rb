39806430c2f656c55d349cb2d3f106fc29f7d13b
Ian Fleeton <ianfleeton@gmail.com>
2015-01-13 19:28:08 +0000

    Fix link to place order

--- spec/views/checkout/index.html.slim_spec.rb
+++ spec/views/checkout/index.html.slim_spec.rb
@@ -2,13 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'checkout/index.html.slim', type: :view do
   before do
-    assign(:address, Address.new)
+    assign(:address, address)
     assign(:basket, Basket.new)
     assign(:discount_lines, [])
     allow(view).to receive(:website).and_return(Website.new)
+    allow(view).to receive(:logged_in?).and_return(false)
   end
 
-  it 'renders' do
-    render
+  context 'with a new address' do
+    let(:address) { Address.new }
+
+    it 'renders' do
+      render
+    end
+  end
+
+  context 'with an existing address' do
+    let(:address) { FactoryGirl.create(:address) }
+
+    it 'renders' do
+      render
+    end
   end
 end
