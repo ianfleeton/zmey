37309e47d2be2d7e4168deae9b55b5354df7b730
Ian Fleeton <ianfleeton@gmail.com>
2015-01-14 17:00:57 +0000

    View for checkout#billing

--- spec/views/checkout/billing.html.slim_spec.rb
+++ spec/views/checkout/billing.html.slim_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'checkout/billing.html.slim', type: :view do
-  it 'renders' do
+  let(:address) { Address.new }
+
+  before do
+    assign(:address, address)
     render
+  end
+
+  it 'has a form to save details' do
+    expect(rendered).to have_selector "form[action='#{addresses_path}'][method='post']"
+  end
+
+  [
+    'address_line_1', 'address_line_2', 'town_city', 'county', 'postcode'
+  ].each do |component|
+    it "has a field for #{component}" do
+      expect(rendered).to have_selector "input[name='address[#{component}]']"
+    end
+  end
+
+  it 'has a select box for country' do
+    expect(rendered).to have_selector "select[name='address[country_id]']"
+  end
+
+  it 'has a submit button' do
+    expect(rendered).to have_selector "input[type='submit']"
   end
 end
