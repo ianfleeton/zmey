67f4d647071cb888b34dd6567f258bee486f9d0f
Ian Fleeton <ianfleeton@gmail.com>
2015-01-23 17:14:03 +0000

    Add include_all parameter to #respond_to?

--- app/models/concerns/extra_attributes.rb
+++ app/models/concerns/extra_attributes.rb
@@ -74,7 +74,7 @@ module ExtraAttributes
     end
   end
 
-  def respond_to?(method_sym)
+  def respond_to?(method_sym, include_all=false)
     ExtraAttribute.method_info(method_sym, self.class)[:exists] || super
   end
 
