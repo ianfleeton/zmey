8db5e62d1ddcecf3784def451fe51a33c82510c5
Ian Fleeton <ianfleeton@gmail.com>
2015-02-13 11:28:06 +0000

    BasketHelper#total_savings

--- app/helpers/basket_helper.rb
+++ app/helpers/basket_helper.rb
@@ -1,4 +1,10 @@
 module BasketHelper
+  # Returns the total savings from RRP reductions and discounts. Incudes tax
+  # if <tt>inc_tax</tt> is true. Returns a positive number for savings made.
+  def total_savings(inc_tax)
+    @basket.savings(inc_tax) + (discount_lines_price_total + (inc_tax ? discount_lines_tax_total : 0)) * -1
+  end
+
   # Returns the total discount amount from discount lines excluding tax.
   def discount_lines_price_total
     @discount_lines.inject(0) { |acc, dl| acc + dl.price_adjustment }
