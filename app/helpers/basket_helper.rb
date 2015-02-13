c1131cb285daca8574b256102142b5e4715e3add
Ian Fleeton <ianfleeton@gmail.com>
2015-02-13 11:18:56 +0000

    Simplify BasketHelper#discount_lines_price_total

--- app/helpers/basket_helper.rb
+++ app/helpers/basket_helper.rb
@@ -1,10 +1,7 @@
 module BasketHelper
+  # Returns the total discount amount from discount lines excluding tax.
   def discount_lines_price_total
-    total = 0
-    @discount_lines.each do |dl|
-      total += dl.price_adjustment
-    end
-    total
+    @discount_lines.inject(0) { |acc, dl| acc + dl.price_adjustment }
   end
 
   def discount_lines_tax_total
