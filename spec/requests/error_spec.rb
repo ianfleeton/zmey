e66b7762821825097c067e61a9ec30f72ee23183
Ian Fleeton <ianfleeton@gmail.com>
2015-02-18 19:56:06 +0000

    Website needed

--- spec/requests/error_spec.rb
+++ spec/requests/error_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe '/error', type: :request do
+  before { FactoryGirl.create(:website) }
+
   it 'raises an exception' do
     expect { get '/error' }.to raise_error
   end
