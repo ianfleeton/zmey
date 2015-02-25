0e29f5caf15707b4ee74fd314a1a323f775084d3
Ian Fleeton <ianfleeton@gmail.com>
2015-02-25 16:42:15 +0000

    Remove image with image picker

---
+++ spec/features/admin/products/edit_product_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'Edit product admin' do
+  background do
+    FactoryGirl.create(:website)
+    sign_in_as_admin
+  end
+
+  scenario 'Remove image from product', js: true do
+    product = FactoryGirl.create(:product, image: FactoryGirl.create(:image))
+    visit edit_admin_product_path(product)
+    click_button 'image_id_image_remove'
+    click_button 'Save'
+    expect(product.reload.image).to be_nil
+  end
+end
