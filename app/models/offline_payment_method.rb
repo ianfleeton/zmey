272b7d47a557817255d8516512cdddfa4a91f95b
Ian Fleeton <ianfleeton@gmail.com>
2015-03-05 15:26:37 +0000

    Validations

--- app/models/offline_payment_method.rb
+++ app/models/offline_payment_method.rb
@@ -1,2 +1,3 @@
 class OfflinePaymentMethod < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
 end
--- spec/models/offline_payment_method_spec.rb
+++ spec/models/offline_payment_method_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe OfflinePaymentMethod, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe OfflinePaymentMethod, type: :model do
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
 end
