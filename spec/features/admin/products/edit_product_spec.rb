aa6ac2bd312927648069ec5ea5c7da130176e7b1
Ian Fleeton <ianfleeton@gmail.com>
2015-03-19 12:41:18 +0000

    Fix image picker specs

--- spec/features/admin/products/edit_product_spec.rb
+++ spec/features/admin/products/edit_product_spec.rb
@@ -9,7 +9,7 @@ feature 'Edit product admin' do
   scenario 'Remove image from product', js: true do
     product = FactoryGirl.create(:product, image: FactoryGirl.create(:image))
     visit edit_admin_product_path(product)
-    click_button 'image_id_image_remove'
+    click_button 'product_image_id_image_remove'
     click_button 'Save'
     expect(product.reload.image).to be_nil
   end
--- spec/features/pages_admin_spec.rb
+++ spec/features/pages_admin_spec.rb
@@ -19,7 +19,7 @@ feature 'Pages admin' do
     fill_in 'Name', with: the_page.name
     fill_in 'Title', with: the_page.title
     fill_in 'Description', with: the_page.description
-    click_button 'thumbnail_image_id_image_picker'
+    click_button 'page_thumbnail_image_id_image_picker'
     click_link "image-#{image.id}"
     click_button 'Create New Page'
     expect(page).to have_content 'added new page'
