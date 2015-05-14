ce8f8a11b97cd0f136fbb62afe933c48814af511
Ian Fleeton <ianfleeton@gmail.com>
2015-05-14 17:31:38 +0100

    Set queue adapter to delayed_job

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module Zmey
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.active_job.queue_adapter = :delayed_job
+
     config.autoload_paths += Dir["#{config.root}/lib"]
     config.autoload_paths << Rails.root.join('app', 'mailers', 'concerns')
 
