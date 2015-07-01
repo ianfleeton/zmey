c039e19b3949b08b24b8ac0974ae1994655f49ae
Ian Fleeton <ianfleeton@gmail.com>
2015-07-01 11:39:06 +0100

    paypal_form_tag

--- app/helpers/checkout_helper.rb
+++ app/helpers/checkout_helper.rb
@@ -1,4 +1,17 @@
 module CheckoutHelper
+  # Renders a form tag for the PayPal payments form. Set
+  # <tt>sandbox</tt> to <tt>true</tt> to send the customer to the
+  # PayPal sandbox (test site), or <tt>false</tt> to send them to
+  # the live site.
+  def paypal_form_tag(sandbox:, &block)
+    url = if sandbox
+            'https://www.sandbox.paypal.com/cgi-bin/webscr'
+          else
+            'https://www.paypal.com/cgi-bin/webscr'
+          end
+    "<form action=\"#{url}\" name=\"_xclick\" method=\"post\">".html_safe + capture(&block) + '</form>'.html_safe
+  end
+
   # Returns an array of hidden field name/value pairs for use in the UPG
   # Atlas payment form.
   def upg_atlas_hidden_fields(order:, checkcode:, shreference:, callbackurl:, filename:, secuphrase:)
--- spec/views/checkout/_paypal.html.slim_spec.rb
+++ spec/views/checkout/_paypal.html.slim_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'checkout/_paypal.html.slim', type: :view do
-  let(:website) { FactoryGirl.create(:website, paypal_active: true, paypal_test_mode: test_mode) }
+  let(:website) { FactoryGirl.create(:website, paypal_active: true, paypal_test_mode: test_mode, paypal_email_address: 'paypal@example.com') }
   let(:order)   { FactoryGirl.create(:order) }
 
   let(:test_mode) { false }
@@ -18,6 +18,13 @@ RSpec.describe 'checkout/_paypal.html.slim', type: :view do
 
   it 'has a hidden notify_url field set to the IPN listener' do
     expect(rendered).to have_selector "input[type=hidden][name=notify_url][value='#{payments_paypal_ipn_listener_url}']"
+  end
+
+  context 'rendered' do
+    subject { rendered }
+    it { should have_selector "input[type=hidden][name=business][value='#{website.paypal_email_address}']"}
+    it { should have_selector "input[type=hidden][name=item_name][value='#{order.order_number}']"}
+    it { should have_selector "input[type=hidden][name=amount][value='#{order.total}']"}
   end
 
   context 'when in test mode (sandbox)' do
