e30ecbe721a00279f11408e7446105b3caabd229
Ian Fleeton <ianfleeton@gmail.com>
2015-07-01 11:15:08 +0100

    Move PayPal form to its own partial

--- spec/views/checkout/_select_payment_method.html.erb_spec.rb
+++ spec/views/checkout/_select_payment_method.html.erb_spec.rb
@@ -38,9 +38,8 @@ RSpec.describe 'checkout/_select_payment_method.html.erb', type: :view do
   end
 
   context 'when PayPal is active' do
-    let(:website) { FactoryGirl.create(:website, paypal_active: true, paypal_test_mode: test_mode) }
+    let(:website) { FactoryGirl.create(:website, paypal_active: true) }
     let(:order)   { FactoryGirl.create(:order) }
-    let(:test_mode) { false }
 
     before do
       allow(view).to receive(:website).and_return(website)
@@ -48,20 +47,8 @@ RSpec.describe 'checkout/_select_payment_method.html.erb', type: :view do
       render
     end
 
-    it 'has a form with PayPal payment page destination' do
-      expect(rendered).to have_selector "form[action='https://www.paypal.com/cgi-bin/webscr']"
-    end
-
-    it 'has a hidden notify_url field set to the IPN listener' do
-      expect(rendered).to have_selector "input[type=hidden][name=notify_url][value='#{payments_paypal_ipn_listener_url}']"
-    end
-
-    context 'when in test mode (sandbox)' do
-      let(:test_mode) { true }
-
-      it 'has a form with PayPal sandbox payment page destination' do
-        expect(rendered).to have_selector "form[action='https://www.sandbox.paypal.com/cgi-bin/webscr']"
-      end
+    it 'renders the PayPal partial' do
+      expect(response).to render_template('_paypal')
     end
   end
 
