1df521666ee56bbcdd222ca0c510de13cd0ed72d
Ian Fleeton <ianfleeton@gmail.com>
2015-07-07 15:48:02 +0100

    Supply website local to view

--- spec/views/products/show.html.erb_spec.rb
+++ spec/views/products/show.html.erb_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 describe 'products/show.html.erb' do
   include ProductsHelper
+  let(:website) { FactoryGirl.build(:website) }
+
   before(:each) do
     assign(:product, FactoryGirl.build(:product))
-    assign(:w, FactoryGirl.build(:website))
+    assign(:w, website)
+    allow(view).to receive(:website).and_return(website)
     allow(view).to receive(:admin_or_manager?).and_return(:false)
   end
 
