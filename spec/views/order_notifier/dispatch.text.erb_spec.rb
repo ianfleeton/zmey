aff6ab7ac102617a9f0c405ff4b16108bcb1b7c7
Ian Fleeton <ianfleeton@gmail.com>
2015-08-07 10:57:25 +0100

    Text version of dispatch email

---
+++ spec/views/order_notifier/dispatch.text.erb_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'order_notifier/dispatch.text.erb', type: :view do
+  let(:website) { FactoryGirl.create(:website) }
+  let(:order) { FactoryGirl.create(:order) }
+  let(:shipment) { FactoryGirl.create(:shipment, order: order) }
+
+  before do
+    assign(:website, website)
+    assign(:order, order)
+    assign(:shipment, shipment)
+  end
+
+  it 'renders' do
+    render
+  end
+end
