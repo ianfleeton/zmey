4bbe12fd1b78f987796fd0e1bc34c9c1edb09a1a
Ian Fleeton <ianfleeton@gmail.com>
2015-10-12 11:01:29 +0100

    Use canonical host as alias doesn't work in Vagrant

--- install-rvm-ruby.sh
+++ install-rvm-ruby.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/vagrant/.rvm/scripts/rvm
 rvm --default use --install 2.2.3
