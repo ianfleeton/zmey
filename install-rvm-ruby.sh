a3301d50fe733eb7fee535f88cce268a5cbbf58c
Ian Fleeton <ianfleeton@gmail.com>
2015-10-12 10:55:35 +0100

    Install Ruby 2.2.3

--- install-rvm-ruby.sh
+++ install-rvm-ruby.sh
@@ -3,4 +3,4 @@
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/vagrant/.rvm/scripts/rvm
-rvm --default use --install 2.1.5
+rvm --default use --install 2.2.3
