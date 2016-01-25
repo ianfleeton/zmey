d6eccffb8a6ad4b9534643e27ba7b7dc34c72d40
Ian Fleeton <ianfleeton@gmail.com>
2016-01-25 17:50:09 +0000

    Import product groups

---
+++ spec/views/admin/import/index.html.slim_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'admin/import/index.html.slim', type: :view do
+  it 'has options for importable classes' do
+    render
+    ['Product', 'ProductGroup'].each do |class_name|
+      expect(rendered).to have_css('option', text: class_name)
+    end
+  end
+end
