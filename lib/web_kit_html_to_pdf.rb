d5438ba76259e5878afb9a7a0cafdea0f6ed6a7f
Ian Fleeton <ianfleeton@gmail.com>
2020-09-21 16:06:35 +0100

    Remove embedded macOS wkhtmltopdf binary

--- lib/web_kit_html_to_pdf.rb
+++ lib/web_kit_html_to_pdf.rb
@@ -1,10 +1,8 @@
+# frozen_string_literal: true
+
 class WebKitHTMLToPDF
   # Returns the appropriate wkhtmltopdf binary for the current platform.
   def self.binary
-    ruby_platform.include?("darwin") ? "./wkhtmltopdf-macosx" : "wkhtmltopdf"
-  end
-
-  def self.ruby_platform
-    RUBY_PLATFORM
+    "wkhtmltopdf"
   end
 end
--- spec/lib/web_kit_html_to_pdf_spec.rb
+++ spec/lib/web_kit_html_to_pdf_spec.rb
@@ -1,20 +1,10 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe WebKitHTMLToPDF do
   describe ".binary" do
-    before do
-      allow(WebKitHTMLToPDF).to receive(:ruby_platform).and_return(ruby_platform)
-    end
     subject { WebKitHTMLToPDF.binary }
-
-    context "RUBY_PLATFORM is x86_64-darwin14" do
-      let(:ruby_platform) { "x86_64-darwin14" }
-      it { should eq "./wkhtmltopdf-macosx" }
-    end
-
-    context "RUBY_PLATFORM is x86_64-linux" do
-      let(:ruby_platform) { "x86_64-linux" }
-      it { should eq "wkhtmltopdf" }
-    end
+    it { should be_instance_of String }
   end
 end
