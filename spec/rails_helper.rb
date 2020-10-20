f107112a1f95771630d180a8fb1b5390c60de65c
Ian Fleeton <ianfleeton@gmail.com>
2020-10-20 11:36:03 +0100

    Make it easy to find flaky tests in a full spec run

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -46,6 +46,15 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.before(:each) do |example|
+    # Make it easy to find flaky tests in a full spec run.
+    Rails.logger.debug("")
+    Rails.logger.debug("@@@@@@@@@@@")
+    Rails.logger.debug("@@ RSPEC @@ #{example.full_description}")
+    Rails.logger.debug("@@@@@@@@@@@")
+    Rails.logger.debug("")
+  end
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
