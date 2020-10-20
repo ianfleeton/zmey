7bb4ae5eb790f6bcf57c977eeeaa05927b31f93d
Ian Fleeton <ianfleeton@gmail.com>
2020-10-20 12:07:31 +0100

    Fix spec

--- spec/system/admin/orders/edit_shipping_details_spec.rb
+++ spec/system/admin/orders/edit_shipping_details_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe "Edit shipping details" do
     fill_in "Shipping method", with: "Long distance courier"
     fill_in "Shipping amount", with: "8"
     fill_in "Shipping tax amount", with: "1.6"
-    click_button "Save"
+    click_save
     order.reload
     expect(order.shipping_method).to eq "Long distance courier"
     expect(order.shipping_amount).to eq 8
@@ -24,8 +24,12 @@ RSpec.describe "Edit shipping details" do
   scenario "Edit delivery instructions" do
     visit edit_admin_order_path(order)
     fill_in "Delivery instructions", with: "Leave in shed"
-    click_button "Save"
+    click_save
     order.reload
     expect(order.delivery_instructions).to eq "Leave in shed"
+  end
+
+  def click_save
+    find(".cancel-save").click_button "Save"
   end
 end
