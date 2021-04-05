42f332a11868936e33bedf946b6c81ce7f898ff7
Ian Fleeton <ianfleeton@gmail.com>
2021-04-05 19:32:30 +0100

    Add slug to products

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_142803) do
+ActiveRecord::Schema.define(version: 2021_04_05_183145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -536,6 +536,8 @@ ActiveRecord::Schema.define(version: 2020_10_29_142803) do
     t.boolean "oversize", default: false, null: false
     t.string "pricing_method", default: "basic", null: false
     t.integer "lead_time", default: 1, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_products_on_slug"
   end
 
   create_table "quantity_prices", id: :serial, force: :cascade do |t|
