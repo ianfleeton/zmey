cad313651d5ccf1508cd8c5a351120cea06f085d
Ian Fleeton <ianfleeton@gmail.com>
2021-08-10 19:42:05 +0100

    Drop delayed_jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_191951) do
+ActiveRecord::Schema.define(version: 2021_08_10_184044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -178,21 +178,6 @@ ActiveRecord::Schema.define(version: 2021_04_05_191951) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["website_id", "path"], name: "index_custom_views_on_website_id_and_path"
-  end
-
-  create_table "delayed_jobs", id: :serial, force: :cascade do |t|
-    t.integer "priority", default: 0, null: false
-    t.integer "attempts", default: 0, null: false
-    t.text "handler", null: false
-    t.text "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string "locked_by"
-    t.string "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
   create_table "discount_uses", force: :cascade do |t|
