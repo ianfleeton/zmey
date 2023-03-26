0528ec2fd3fd98e8d99410e9a4d35a01f8855f3a
Ian Fleeton <ianfleeton@gmail.com>
2023-03-26 13:40:34 +0100

    Ruby 3.1.3

--- install-rvm-ruby.sh
+++ install-rvm-ruby.sh
@@ -3,4 +3,4 @@
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/vagrant/.rvm/scripts/rvm
-rvm --default use --install 2.7.2
+rvm --default use --install 3.1.3
