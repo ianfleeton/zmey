6c330c7657c4eb6d4d74b0b78c98386c00d8634d
Ian Fleeton <ianfleeton@gmail.com>
2024-12-11 20:25:40 +0000

    Add /up for health check status

--- config/routes.rb
+++ config/routes.rb
@@ -297,6 +297,10 @@ Rails.application.routes.draw do
   # exception reporting.
   get 'error' => 'application#error'
 
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
   SLUG_CONSTRAINTS = {slug: /[-a-zA-Z0-9_\/\.]*/}
   get "product/:slug" => "products#show", constraints: SLUG_CONSTRAINTS
   get ':slug' => 'pages#show', as: :slug, constraints: SLUG_CONSTRAINTS
