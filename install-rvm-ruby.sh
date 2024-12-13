86121208ef24c8e91930ef91c9fee003d5bbe35b
Ian Fleeton <ianfleeton@gmail.com>
2024-12-13 10:34:08 +0000

    Remove Vagrant

--- install-rvm-ruby.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
-source /home/vagrant/.rvm/scripts/rvm
-rvm --default use --install 3.3.6
